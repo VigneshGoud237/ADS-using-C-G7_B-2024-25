77116745d297f9134429a744a0db20fd50c1682a
VigneshGoud237 <2311cs010237@mallareddyuniversity.ac.in>
2025-01-09 12:36:33 +0530

    yes commit

---
+++ chrep.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    char ch = 49;
+    cout << ch << endl;
+    cout << +ch << endl;
+    char ch2 = 197;
+    cout << ch2;
+    return 0;
+}
